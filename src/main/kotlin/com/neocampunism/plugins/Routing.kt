069dca64e8f002bd49b6cc357c177b703a272fae
Wahid Sadique Ayan <wsayan28@gmail.com>
2024-09-16 15:32:29 +0600

    add semester routes

--- src/main/kotlin/com/neocampunism/plugins/Routing.kt
+++ src/main/kotlin/com/neocampunism/plugins/Routing.kt
@@ -1,14 +1,10 @@
 package com.neocampunism.plugins
 
 import com.neocampunism.repository.course.CourseRepositoryImpl
-import com.neocampunism.repository.course.FakeCourseRepository
 import com.neocampunism.repository.department.DepartmentRepositoryImpl
-import com.neocampunism.repository.department.FakeDepartmentRepository
-import com.neocampunism.repository.professor.FakeProfessorRepository
-import com.neocampunism.repository.professor.ProfessorRepository
 import com.neocampunism.repository.professor.ProfessorRepositoryImpl
-import com.neocampunism.repository.room.FakeRoomRepository
 import com.neocampunism.repository.room.RoomRepositoryImpl
+import com.neocampunism.repository.semester.SemesterRepositoryImpl
 import com.neocampunism.repository.time_slot.FakeTimeSlotRepository
 import com.neocampunism.routing.*
 import com.neocampunism.response.ApiResponse
@@ -17,6 +13,7 @@ import com.neocampunism.service.course.CourseServiceImpl
 import com.neocampunism.service.department.DepartmentServiceImpl
 import com.neocampunism.service.professor.ProfessorServiceImpl
 import com.neocampunism.service.room.RoomServiceImpl
+import com.neocampunism.service.semester.SemesterServiceImpl
 import com.neocampunism.service.time_slot.TimeSlotServiceImpl
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -28,55 +25,53 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    install(StatusPages) {
-        exception<Throwable> { call, cause ->
-            when (cause) {
-                is NotFoundException -> {
-                    call.respond(
-                        HttpStatusCode.NotFound,
-                        ApiResponse(
-                            status = "failed",
-                            message = "Resource not found",
-                            data = ErrorBody(cause = cause.localizedMessage)
+    routing {
+        install(StatusPages) {
+            exception<Throwable> { call, cause ->
+                when (cause) {
+                    is NotFoundException -> {
+                        call.respond(
+                            HttpStatusCode.NotFound,
+                            ApiResponse(
+                                status = "failed",
+                                message = "Resource not found",
+                                data = ErrorBody(cause = cause.localizedMessage)
+                            )
                         )
-                    )
-                }
+                    }
 
-                is BadRequestException -> {
-                    call.respond(
-                        HttpStatusCode.BadRequest, ApiResponse(
-                            status = "failed",
-                            message = "Invalid request",
-                            data = ErrorBody(cause = cause.localizedMessage)
+                    is BadRequestException -> {
+                        call.respond(
+                            HttpStatusCode.BadRequest, ApiResponse(
+                                status = "failed",
+                                message = "Invalid request",
+                                data = ErrorBody(cause = cause.localizedMessage)
+                            )
                         )
-                    )
-                }
+                    }
 
-                else -> {
-                    call.respond(
-                        HttpStatusCode.InternalServerError,
-                        ApiResponse(
-                            status = "failed",
-                            message = "An unexpected error occurred",
-                            data = ErrorBody(cause = cause.localizedMessage)
+                    else -> {
+                        call.respond(
+                            HttpStatusCode.InternalServerError,
+                            ApiResponse(
+                                status = "failed",
+                                message = "An unexpected error occurred",
+                                data = ErrorBody(cause = cause.localizedMessage)
+                            )
                         )
-                    )
 
-                    logError(call, cause)
+                        logError(call, cause)
+                    }
                 }
             }
         }
-    }
 
-    routing {
-        routing {
-            swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
-        }
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
 
         departmentRoutes(DepartmentServiceImpl(DepartmentRepositoryImpl()))
         professorRoutes(ProfessorServiceImpl(ProfessorRepositoryImpl()))
         courseRoutes(CourseServiceImpl(CourseRepositoryImpl()))
-        timeSlotRoutes(TimeSlotServiceImpl(FakeTimeSlotRepository()))
+        semesterRoutes(SemesterServiceImpl(SemesterRepositoryImpl()))
         roomRoutes(RoomServiceImpl(RoomRepositoryImpl()))
     }
 }
--- src/main/kotlin/com/neocampunism/routing/SemesterRoutes.kt
+++ src/main/kotlin/com/neocampunism/routing/SemesterRoutes.kt
@@ -1,49 +1,50 @@
 package com.neocampunism.routing
 
+import com.neocampunism.model.Semester
 import com.neocampunism.model.TimeSlot
 import com.neocampunism.response.ApiResponse
-import com.neocampunism.service.time_slot.TimeSlotService
+import com.neocampunism.service.semester.SemesterService
 import io.ktor.http.*
 import io.ktor.server.request.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 
-fun Route.timeSlotRoutes(timeSlotService: TimeSlotService) {
-    route("/timeslots") {
+fun Route.semesterRoutes(semesterService: SemesterService) {
+    route("/semesters") {
 
         post {
-            val timeSlot = call.receive<TimeSlot>()
-            val response = timeSlotService.createTimeSlot(timeSlot)
+            val semester = call.receive<Semester>()
+            val response = semesterService.createSemester(semester)
             call.respond(HttpStatusCode.Created, response)
         }
 
         get {
-            val response = timeSlotService.getTimeSlots()
+            val response = semesterService.getSemesters()
             call.respond(HttpStatusCode.OK, response)
         }
 
         get("/{id}") {
             val id = call.parameters["id"]?.toIntOrNull() ?: return@get call.respond(HttpStatusCode.BadRequest,
-                ApiResponse<Any>(status = "failed", message = "Invalid time slot ID")
+                ApiResponse<Any>(status = "failed", message = "Invalid semester ID")
             )
-            val response = timeSlotService.getTimeSlot(id)
+            val response = semesterService.getSemester(id)
             call.respond(HttpStatusCode.OK, response)
         }
 
         put("/{id}") {
             val id = call.parameters["id"]?.toIntOrNull() ?: return@put call.respond(HttpStatusCode.BadRequest,
-                ApiResponse<Any>(status = "failed", message = "Invalid time slot ID")
+                ApiResponse<Any>(status = "failed", message = "Invalid semester ID")
             )
-            val updatedTimeSlot = call.receive<TimeSlot>()
-            val response = timeSlotService.updateTimeSlot(id, updatedTimeSlot)
+            val updatedSemester = call.receive<Semester>()
+            val response = semesterService.updateSemester(id, updatedSemester)
             call.respond(HttpStatusCode.OK, response)
         }
 
         delete("/{id}") {
             val id = call.parameters["id"]?.toIntOrNull() ?: return@delete call.respond(HttpStatusCode.BadRequest,
-                ApiResponse<Any>(status = "failed", message = "Invalid time slot ID")
+                ApiResponse<Any>(status = "failed", message = "Invalid semester ID")
             )
-            val response = timeSlotService.deleteTimeSlot(id)
+            val response = semesterService.deleteSemester(id)
             call.respond(HttpStatusCode.OK, response)
         }
     }
