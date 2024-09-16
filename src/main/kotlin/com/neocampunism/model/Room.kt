610e70d79eb9f8b5aa33a9646c2e37bde7bdf48e
Wahid Sadique Ayan <wsayan28@gmail.com>
2024-09-16 23:27:47 +0600

    minor update

--- src/main/kotlin/com/neocampunism/model/Room.kt
+++ src/main/kotlin/com/neocampunism/model/Room.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Room(
-    val roomID: Int?= null,
-    val roomNumber: String?= null,
-    val capacity: Int?= null,
-    val roomType: RoomType?= null,
+    val roomID: Int? = null,
+    val roomNumber: String? = null,
+    val capacity: Int? = null,
+    val roomType: RoomType? = null,
 )
