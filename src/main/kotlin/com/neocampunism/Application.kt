07af47f9bfe8677ca61d631d4dca471ec4adcf2e
Wahid Sadique Ayan <wsayan28@gmail.com>
2024-09-18 01:19:33 +0600

    remove unnecessary comments

--- src/main/kotlin/com/neocampunism/Application.kt
+++ src/main/kotlin/com/neocampunism/Application.kt
@@ -2,13 +2,6 @@ package com.neocampunism
 
 import com.neocampunism.plugins.*
 import io.ktor.server.application.*
-/*import io.ktor.server.engine.*
-import io.ktor.server.netty.**/
-
-/*fun main() {
-    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
-        .start(wait = true)
-}*/
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
